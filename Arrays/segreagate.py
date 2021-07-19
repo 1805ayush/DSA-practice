e7d3a186df06c90e8980a4d44f331b481dd0632f
Ayush <ayush.chakladar@gmail.com>
2021-07-19 23:41:36 +0530

    segregate in python

---
+++ Arrays/segreagate.py
@@ -0,0 +1,13 @@
+arr=[1,7,-1,2,4,-5,-7,11]
+pos =[]
+neg =[]
+        
+for item in arr:
+    if(item>0):
+        pos.append(item)
+    elif(item<0):
+       neg.append(item)
+    
+pos.extend(neg)
+arr = pos
+print(arr)
